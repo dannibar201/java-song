4dc0235f17982eb365c0670002eacf2ec30602fc
20176 <20176@MININT-VTTS8LK.columbushs.com>
2019-09-18 14:18:23 -0400

    Done

--- song/src/song/Song.java
+++ song/src/song/Song.java
@@ -15,15 +15,33 @@ public class Song {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // Code inside the main method
-        
-        
-        
-            
+        System.out.println("Don't let them fool ya \n"
+                + "Or even try to school ya \n "
+                + "We've got a mind of our own \n"
+                + "So go to hell if what you're thiking is not right \n"
+                + "Love would never leave us alone \n"
+                + "'Ey in the darkness there must come out to light ");
+        chorus();
+        System.out.println("The road of life is rocky and you may stumble too \n"
+                + "So while you point your fingers \n"
+                + "Someone else is judging you \n"
+                + "Love your brother-man");
+        chorus();
+        System.out.println("Don't let them change ya \n"
+                + "Oh, no \n"
+                + "We've got a life to live \n"
+                + "They say \n"
+                + "Only,only \n"
+                + "Only the fittest of the fittest shall survive \n"
+                + "Stay alive");
     }
-    
-    public static void chorus(){
-        // Chorus lyrics go here
+
+    public static void chorus() {
+        System.out.println();
+        System.out.println("Could you be loved and be loved?");
+        System.out.println("Could you be loved and be loved?");
+        System.out.println();
     }
-    
 }
+
+// Code inside the main method
