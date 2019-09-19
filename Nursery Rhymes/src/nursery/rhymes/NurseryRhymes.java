7c6382f5d9b938577ce4878027784dca33a9fbc4
20176 <20176@MININT-VTTS8LK.columbushs.com>
2019-09-19 08:51:48 -0400

    Class Ended

--- Nursery Rhymes/src/nursery/rhymes/NurseryRhymes.java
+++ Nursery Rhymes/src/nursery/rhymes/NurseryRhymes.java
@@ -15,33 +15,40 @@ public class NurseryRhymes {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+
         farm("cow", "moo");
-        farm("duck", "quack");        
+//        farm("duck", "quack"); 
+//        farm("cat", "meow");
         // TODO: add another animal to the farm here
-        hickory_dickory(1);
-        hickory_dickory(2);
+//        monkeys(10);
+//        hickory_dickory(1);
+//        hickory_dickory(2);
         // TODO: make the clock strike three here
-        
+
         // TODO: call your new methods here ( you must write them first! ) 
-        
-        
     }
-    
-    
-    
-    public static void farm(String animal, String sound){
+
+    public static void farm(String animal, String sound) {
+        System.out.println("Old MacDonald had a farm\n"
+                + "e-i-e-i-0\n"
+                + "And on that farm he had a " + animal + "\n"
+                + "e-i-e-i-o\n"
+                + "With a " + sound + " " + sound + " here \n"
+                + "And a " + sound + " " + sound + " there \n"
+                + "Here a " + sound + " there a " + sound + "\n"
+                + "Verywhere a " + sound + " " + sound + "\n"
+                + "Old MacDonald had a farm\n"
+                + "e-i-e-i-o");
+
+    }
+
+    public static void monkeys(int number) {
         // TODO: write your code here
     }
-    
-    public static void monkeys(int number){
+
+    public static void hickory_dickory(int time) {
         // TODO: write your code here
     }
-    
-    public static void hickory_dickory(int time){
-        // TODO: write your code here
-    }
-    
+
     // TODO add your new methods here
-    
 }
